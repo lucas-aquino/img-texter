c5cab77c35e5ad8e590ac4bde041a86db81c7bc1
lucas-aquino <luckasakino@protonmail.com>
2021-09-30 19:17:06 -0300

    Se Agrego el git ignore

--- src/App.py
+++ src/App.py
@@ -2,16 +2,22 @@ import tkinter
 
 
 class App(tkinter.Frame):
-    def __init__(self, master):
+    def __init__(self, master, title = "IMG Texter"):
         super().__init__(master)
         self.master = master
+        self.setTitle(title = title)
         self.pack()
+
+    def setTitle(self, title : str):
+        self.master.title(title)
 
 
 app = App(master = tkinter.Tk())
 
 
-app.master.title("IMG Texter")
+#app.setTitle(title = "IMG Texter")
+
+#app.master.title("IMG Texter")
 app.master.geometry("400x300")
 
 def hello(nombre):
